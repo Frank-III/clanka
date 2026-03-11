ae7e265760b37f10ba6aa4c03fe057a7dfa03aff
Tim Smart <hello@timsmart.co>
2026-03-11 21:00:17 +1300

    wip

--- scripts/worktree-setup.sh
+++ scripts/worktree-setup.sh
@@ -5,6 +5,4 @@ corepack install
 pnpm install
 
 git clone https://github.com/effect-ts/effect-smol.git --depth 1 .repos/effect
-git clone https://github.com/effect-ts/content.git --depth 1 .repos/content
 git clone https://github.com/anomalyco/opencode.git --depth 1 .repos/opencode
-git clone https://github.com/tim-smart/lalph.git --depth 1 .repos/lalph
